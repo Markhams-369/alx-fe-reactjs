aa8b0e179d19e8b49c89d79b850cbfad3a0e6ade
MarkhamsVision <tboitumelo868@gmail.com>
2026-02-14 02:50:09 +0200

    Router implementation

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
 import FavoritesList from './components/FavoritesList';
@@ -51,6 +51,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Router>
         <div style={{ padding: '2rem', maxWidth: '700px', margin: '0 auto' }}>
       <h1>Recipe Sharing App</h1>
       <nav style={{ marginBottom: "20px" }}>
@@ -74,6 +75,7 @@ function App() {
         <Route path="/recommendations" element={<RecommendationsList />} />
       </Routes>
     </div>
+    </Router>
     </>
   )
 }
