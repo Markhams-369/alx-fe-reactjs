f701aceb9a1789b5096a879572d0f1c144a381b6
Boitumelo Markhams <tboitumelo868@gmail.com>
2026-02-26 11:04:51 +0200

    config

--- github-user-search/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export const content = ["./index.html", "./src/**/*.{js,jsx,ts,tsx,css}"];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
-
